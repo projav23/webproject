7103afb4445f4bb05767610ceccd6381c2e87734
Tomy Tubert <tomytubert@gmail.com>
2020-12-22 10:07:18 +0100

    Done nav bar responsive for all devices, also did the menu partial but not added on the hbs

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Para decirle donde estan los partials
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
+
+
 //Routas
 const index = require('./routes/index.routes')
 const login = require('./routes/login.routes')
