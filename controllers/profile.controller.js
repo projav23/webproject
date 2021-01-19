679a3e42c30eb50c70c9509418094dc4f77c8db2
projav <projav23@gmail.com>
2021-01-19 17:26:42 +0100

    done errors and bugs

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -12,16 +12,17 @@ const getProfile = async (req, res, next) => {
   const {userId} = req.params
   const user = await Users.findById(userId)
   //Data para el grafico
-  const chartGames = await Matches.find({$and: [{acceptedGuests:{$in: req.session.currentUser._id}},{status: "Finalizado"}]})
+  const chartGames = await Matches.find({$and: [{acceptedGuests:{$in: userId}},{status: "Finalizado"}]})
   const chartScore = await Users.findById(userId, {scoreInd: 1, scoreDob: 1})
   console.log(chartGames.length)
   console.log(chartScore.scoreInd)
   //Estadistica ultimos partidos
-  const matchStats = await Matches.find({$and: [{acceptedGuests:{$in: req.session.currentUser._id}},{status: "Finalizado"}]}).sort({date:-1}).limit(10)
+  const matchStats = await Matches.find({$and: [{acceptedGuests:{$in: userId}},{status: "Finalizado"}]}).sort({date:-1}).limit(10)
   const topScoreInd = await Users.find({},{scoreInd:1, _id: 1, username: 1} ).sort({scoreInd: -1})
   const topScoreDob = await Users.find({},{scoreDob:1, _id: 1, username: 1} ).sort({scoreDob: -1})
-  const posInd = topScoreInd.findIndex(x => x.id === req.session.currentUser._id) + 1
-  const posDob = topScoreDob.findIndex(x => x.id === req.session.currentUser._id) + 1
+  const posInd = topScoreInd.findIndex(x => x.id === userId) + 1
+  const posDob = topScoreDob.findIndex(x => x.id === userId) + 1
+  //Solicitudes
   const matches = await Matches.find({$and: [{host: req.session.currentUser._id}, {pendingGuests: {$ne: []}}]}).populate('pendingGuests center', {passwordHash: 0})
   if(matches.length === 0){
     res.render('profile', {message: "No tienes solicitudes pendientes", user, matchStats, posInd, posDob, chartGames, chartScore})
