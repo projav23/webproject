faa753928435cf0cd9bf6c03c48441bd8a8bb194
projav <projav23@gmail.com>
2021-01-22 00:53:35 +0100

    done fix error profile

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -11,9 +11,50 @@ const templates = require('../public/templates/template')
 const getProfile = async (req, res, next) => {
   try {
   const {userId} = req.params
-  const user = await Users.findById(userId)
+  const user = await Users.findById(userId).populate("comunidad")
   //Comunidades
-  const comunidades = await Comunidades.find()
+  const comunidades = await Comunidades.find().populate("comunidad")
+  //Data para el grafico Tenis individual
+  const chartGames = await Matches.find({$and: [{acceptedGuests:{$in: userId}},{status: "Finalizado"}, {esport: "Tenis"}, {numberPlayers: 2}]})
+  //Data para el grafico Tenis dobles
+  const chartGamesDob = await Matches.find({$and: [{acceptedGuests:{$in: userId}},{status: "Finalizado"}, {esport: "Tenis"}, {numberPlayers: 4}]})
+  //Puntuacion tenis
+  const chartScore = await Users.findById(userId, {scoreTenisInd: 1, scoreTenisDob: 1})
+  //Data para el grafico Padel individual
+  const chartGamesPadel = await Matches.find({$and: [{acceptedGuests:{$in: userId}},{status: "Finalizado"}, {esport: "Padel"}]})
+  //Data para el grafico padel dobles
+  const chartGamesPadelDob = await Matches.find({$and: [{acceptedGuests:{$in: userId}},{status: "Finalizado"}, {esport: "Padel"}]})
+  //Puntuacion padel
+  const chartScorePadel = await Users.findById(userId, {scorePadelInd: 1, scorePadelDob: 1})
+  //Estadistica ultimos partidos Tenis
+  const matchStats = await Matches.find({$and: [{acceptedGuests:{$in: userId}},{status: "Finalizado"}, {esport: "Tenis"}]}).sort({date:-1}).limit(10)
+  const topScoreInd = await Users.find({},{scoreTenisInd:1, _id: 1, username: 1} ).sort({scoreTenisInd: -1})
+  const topScoreDob = await Users.find({},{scoreTenisDob:1, _id: 1, username: 1} ).sort({scoreTenisDob: -1})
+  const posInd = topScoreInd.findIndex(x => x.id === userId) + 1
+  const posDob = topScoreDob.findIndex(x => x.id === userId) + 1
+  //Estadistica ultimos partidos Padel
+  const matchStatsPadel = await Matches.find({$and: [{acceptedGuests:{$in: userId}},{status: "Finalizado"}, {esport: "Padel"}]}).sort({date:-1}).limit(10)
+  const topScoreIndPadel = await Users.find({},{scorePadelInd:1, _id: 1, username: 1} ).sort({scorePadelInd: -1})
+  const topScoreDobPadel = await Users.find({},{scorePadelDob:1, _id: 1, username: 1} ).sort({scorePadelDob: -1})
+  const posIndPadel = topScoreIndPadel.findIndex(x => x.id === userId) + 1
+  const posDobPadel = topScoreDobPadel.findIndex(x => x.id === userId) + 1
+  //Solicitudes
+  const matches = await Matches.find({$and: [{host: userId}, {pendingGuests: {$ne: []}}]}).populate('pendingGuests center', {passwordHash: 0})
+  if(matches.length === 0){
+    res.render('profile', {message: "No tienes solicitudes pendientes", user, matchStats, posInd, posDob, chartGames, chartScore, userId, chartGamesPadel,chartScorePadel,matchStatsPadel, posIndPadel, posDobPadel, chartGamesDob, chartGamesPadelDob, comunidades})
+  } else {
+  res.render('profile', {matches, user, matchStats, posInd, posDob, chartGames, chartScore, userId, chartGamesPadel,chartScorePadel,matchStatsPadel, posIndPadel, posDobPadel, chartGamesDob, chartGamesPadelDob, comunidades})
+  }
+  } catch (e) {
+  console.error(e)
+  }
+}
+const getRequest = async (req, res, next) => {
+  try {
+  const {userId} = req.params
+  const user = await Users.findById(userId).populate("comunidad")
+  //Comunidades
+  const comunidades = await Comunidades.find().populate("comunidad")
   //Data para el grafico Tenis individual
   const chartGames = await Matches.find({$and: [{acceptedGuests:{$in: userId}},{status: "Finalizado"}, {esport: "Tenis"}, {numberPlayers: 2}]})
   //Data para el grafico Tenis dobles
@@ -50,6 +91,8 @@ const getProfile = async (req, res, next) => {
   }
 }
 
+
+
 const editProfile = async (req,res,next) => {
   try {
     const {userId} = req.params;
@@ -65,7 +108,7 @@ const editProfile = async (req,res,next) => {
       },
       {new:true}
       )
-      res.redirect(`/profile/${userId}`)
+      res.redirect(`/profile/${userId}/#Perfil`)
   } catch (e) {
   console.error(e)
   }
@@ -100,4 +143,4 @@ const sendEmail = async (req,res,next) => {
   }
 }
 
-module.exports = {getProfile, editProfile, sendEmail}
+module.exports = {getProfile, editProfile, sendEmail, getRequest}
--- controllers/solicitudes.controller.js
+++ controllers/solicitudes.controller.js
@@ -2,19 +2,19 @@ const Users = require('../models/User.model')
 const Matches = require('../models/Matches.model')
 const mongoose = require('mongoose')
 
-const getRequests = async (req,res, next) => {
-  try {
-    const matches = await Matches.find({$and: [{host: req.session.currentUser._id}, {pendingGuests: {$ne: []}}]}).populate('pendingGuests', `username`)
+// const getRequests = async (req,res, next) => {
+//   try {
+//     const matches = await Matches.find({$and: [{host: req.session.currentUser._id}, {pendingGuests: {$ne: []}}]}).populate('pendingGuests', `username`)
 
-    if(matches.length === 0){
-      res.render('solicitudes', {message: "No tienes solicitudes pendientes"})
-    } else {
-    res.render('solicitudes', {matches})
-    }
-  } catch (error) {
-    console.error(error)
-  }
-}
+//     if(matches.length === 0){
+//       res.render('solicitudes', {message: "No tienes solicitudes pendientes"})
+//     } else {
+//     res.render('solicitudes', {matches})
+//     }
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
 
 const acceptedGuest = async (req, res, next) => {
   try {
@@ -37,7 +37,7 @@ const acceptedGuest = async (req, res, next) => {
     },
     {new: true}
     )
-  res.redirect(`/profile/${req.session.currentUser._id}`)
+  res.redirect(`/profile/${req.session.currentUser._id}/#Solicitudes`)
   } catch (e) {
   console.error(e)
   }
@@ -57,10 +57,10 @@ const declineGuest = async (req, res, next) => {
     {$pull: {pendingEvents: matchId}},
     {new: true}
     )
-    res.redirect(`/profile/${req.session.currentUser._id}`)
+    res.redirect(`/profile/${req.session.currentUser._id}/#Solicitudes`)
   } catch (e) {
   console.error(e)
   }
 }
 
-module.exports = {getRequests, acceptedGuest, declineGuest}
+module.exports = {acceptedGuest, declineGuest}
