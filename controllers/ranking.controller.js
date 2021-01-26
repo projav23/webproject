fab3adff72b95b79f93bda5464857291fc3fec40
Tomy Tubert <tomytubert@gmail.com>
2021-01-26 16:49:50 +0100

    Done all table ranking

--- controllers/ranking.controller.js
+++ controllers/ranking.controller.js
@@ -7,16 +7,24 @@ const mongoose = require('mongoose')
 
 const getRanking = async (req,res,next) => {
   try {
+  const {userId} = req.params
+  user = await Users.findById(userId)
   const rankingInd = await Users.find().sort({scoreTenisInd: -1}).populate("comunidad")
-  console.log("RankingInd", rankingInd[0])
-  console.log("Comunidad",rankingInd[2])
+  //Posición del ranking
+  //Tenis
+  const topScoreInd = await Users.find({},{scoreTenisInd:1, _id: 1, username: 1} ).sort({scoreTenisInd: -1})
+  const posInd = topScoreInd.findIndex(x => x.id === userId) + 1
+  const noTop = posInd > 3
+  // console.log("pos",posInd);
+  // console.log("noTop",noTop);
   const rankingDob = await Users.find().sort({scoreTenisDob: -1}).populate("comunidad")
   const rankingIndPadel = await Users.find().sort({scorePadelInd: -1}).populate("comunidad")
   const rankingDobPadel = await Users.find().sort({scorePadelDob: -1}).populate("comunidad")
-  res.render("ranking", {rankingInd, rankingDob, rankingIndPadel, rankingDobPadel})
+  res.render("ranking", {rankingInd, rankingDob, rankingIndPadel, rankingDobPadel, userId, user,posInd,noTop})
   } catch (e) {
   console.error(e)
   }
 }
+
 
 module.exports = {getRanking}
